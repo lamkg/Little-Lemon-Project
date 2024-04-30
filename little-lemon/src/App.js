3741a19536c8084c288a00030c792d01b206e639
lamkg <lamkevin93@gmail.com>
2024-04-30 15:05:26 -0700

    test commit on nav bar progress

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,22 +1,16 @@
+import React from 'react';
 import './App.css';
+import Nav from "./components/Nav"
+// import Main from "./components/Main"
+// import Menu from './components/Menu';
+// import Footer from './components/Footer';
+
 
 function App() {
   return (
+ 
     <>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
     </>
   );
 }
---
+++ little-lemon/src/components/Nav.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import logo from '../little-lemon-assets/LemonLogo.png'
+
+const Nav = () => {
+    return (
+        <img src ={logo} alt ='picture of lemon logo'/>
+    )
+}
+
+export default Nav;
